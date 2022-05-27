805c41a4ef8170ecd9ea7fc5fbf4554842be1155
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-27 15:59:20 +0530

    Add move and steer Driver.cs

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] private float steerSpeed = 1f;
+    [SerializeField] private float moveSpeed = 0.01f;
 
     void Start()
     {
-        
     }
-
 
     void Update()
     {
-        
+        transform.Rotate(0f, 0f, steerSpeed);
+        transform.Translate(0f, moveSpeed, 0f);
     }
 } // Class
