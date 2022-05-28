9f41032bd726cc0a5ae1151f7415990daeba0ad0
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-28 14:02:53 +0530

    Add City Limits

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -11,8 +11,8 @@ public class Delivery : MonoBehaviour
     [SerializeField] private Color32 noPackageColor = new Color32(1, 1, 1, 1);
 
     private SpriteRenderer _sp;
-
-    [SerializeField] private bool hasPackage = false;
+    private bool hasPackage = false;
+    
     [SerializeField] private GameObject packagePrefab;
 
     private void OnCollisionEnter2D(Collision2D col)
@@ -27,12 +27,15 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package Pickup");
             Destroy(collidedObject.GameObject(), 1f);
             hasPackage = true;
+            _sp.color = hasPackageColor;
         }
         else if (collidedObject.CompareTag("Customer") && hasPackage)
         {
             Debug.Log("Package Delivered");
-            // Destroy(collidedObject.GameObject(), 2f);
+            _sp.color = noPackageColor;
             hasPackage = false;
+
+            // Destroy(collidedObject.GameObject(), 2f);
             //collidedObject.GetComponent<Transform>().position
         }
     }
