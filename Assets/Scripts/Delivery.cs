aea4011db74b39686d8bc1c946d28327dbbdafc4
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-28 11:52:55 +0530

    Add Delivery.cs , tags

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D col)
     {
