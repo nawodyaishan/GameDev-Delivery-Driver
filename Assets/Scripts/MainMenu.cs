0da981d773b498390baa712f0ec66f0fc7f5d15c
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-30 14:17:41 +0530

    Add How To Play UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] GameObject howToPlay;
+    
+    private void Start()
+    {
+        howToPlay.SetActive(false);
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene("Gameplay");
+    }
+
+    public void HowToPlay()
+    {
+        howToPlay.SetActive(true);
+    }
+    
+    public void Back()
+    {
+        howToPlay.SetActive(false);
     }
 
     public void Exit()
